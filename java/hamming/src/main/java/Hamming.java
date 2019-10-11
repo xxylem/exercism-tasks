93474017482d27253526c3de0b9eecb3db10cb53
xxylem <35666594+xxylem@users.noreply.github.com>
2019-10-11 13:18:32 +0100

    Implement Hamming (Java) s.t. all tests pass.

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,11 +1,52 @@
 class Hamming {
 
+    private final String leftStrand;
+    private final String rightStrand;
+    private final int len;
+    private int hammingDistance;
+
     Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        int lenL = leftStrand.length();
+        int lenR = rightStrand.length();
+
+        // Sanity check on arguments.
+        if (lenL == 0 && lenR > 0) {
+            throw new IllegalArgumentException(
+                "left strand must not be empty.");
+        }
+        else if (lenR == 0 && lenL > 0) {
+            throw new IllegalArgumentException(
+                "right strand must not be empty.");
+        }
+        else if (lenL != lenR) {
+            throw new IllegalArgumentException(
+                "leftStrand and rightStrand must be of equal length.");
+        }
+
+        this.len = lenL;
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
+        computeHammingDistance();
+
     }
 
-    int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    // Getters
+    int getHammingDistance() { return hammingDistance; } 
+    String getLeftStrand() { return leftStrand; } 
+    String getRightStrand() { return rightStrand; } 
+
+    void computeHammingDistance() {
+        
+        int dist = 0;
+
+        for (int i = 0; i < len; i++) {
+            if (leftStrand.charAt(i) != rightStrand.charAt(i)){
+                dist += 1;
+            }
+        }
+
+        hammingDistance = dist;
     }
 
 }
