e5fc79ff69b78deb5f3b7d44df73a73a852be32f
xxylem <35666594+xxylem@users.noreply.github.com>
2019-10-12 18:41:22 +0100

    Delete old word-count. Add in new version.

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,54 +1,22 @@
-def add_word_to_dict(words_found, current_word):
-    '''
-    Input: words_found (dictionary), current_word (string) to add to dictionary
-    Only adds the current word if it has data, otherwise does nothing.
-    Always returns 0
-    '''
-    if len(current_word) != 0:
-            if current_word in words_found:
-                words_found[current_word] += 1
-            else:
-                words_found[current_word] = 1
-    return 0
+import string, re, collections
 
-def word_count(phrase):
+
+def count_words(phrase):
     '''
     Given a phrase, count the occurrences of each word in that phrase.
     Input: phrase (string) to test
     Output: dictionary of words found with number of occurrences associated with each
     '''
     
-    words_found = {}
-    current_word = ''
-    length_phrase = len(phrase)
-    i = 0 # index for loop
-    
-    while i < length_phrase:
+    # Use regex to split the phrase by commas, underscores and whitespace.
+    # Strip any extra punctuation around the words and put everything in lowercase.
+    words = [word.strip(string.punctuation).lower() for word in 
+                re.split(f"[,_{string.whitespace}]", phrase)]
+
+    # Build the count of each word in the phrase.
+    word_counter = collections.Counter()
+    for word in words:
+        if word:
+            word_counter[word] += 1
         
-        if phrase[i].isdigit(): # deals with digits
-            current_word += phrase[i] 
-            
-        elif phrase[i].isalpha(): # deals with letters
-            current_word += phrase[i].lower()
-            
-        elif phrase[i] == '\'': # deals with apostrophes
-            if i + 1 >= length_phrase: # avoids attempting to index out of range
-                pass
-            elif current_word == '': # ignores leading apostrophes from words
-                pass
-            elif phrase[i + 1].isalpha():
-                # adds apostrophe to word only if it is in the middle of a word
-                current_word += phrase[i].lower()
-            # Other possibilities are passed over
-        else:
-            # Adds word to dict only if there is something to add, then
-            # resets the current word
-            add_word_to_dict(words_found, current_word)
-            current_word = ''
-        i += 1 # Increment index for while loop (for loop also possible, but since
-                # we need to look around the current index, while seemed easier
-        
-    # To account for final word in phrase
-    add_word_to_dict(words_found, current_word)
-    
-    return words_found
+    return dict(word_counter)
