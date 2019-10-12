7632dfc4d9dd9acc96c1c2fd941c474848e88c9d
xxylem <35666594+xxylem@users.noreply.github.com>
2019-10-12 18:40:25 +0100

    Use library Counter instead of custom function and dictionary to count occurences of words.

--- python/word-count-2/word_count.py
+++ python/word-count-2/word_count.py
@@ -1,17 +1,5 @@
-import string, re
+import string, re, collections
 
-def add_word_to_dict(words_found, current_word):
-    '''
-    Input: words_found (dictionary), current_word (string) to add to dictionary
-    Only adds the current word if it has data, otherwise does nothing.
-    Always returns 0
-    '''
-    if len(current_word) != 0:
-            if current_word in words_found:
-                words_found[current_word] += 1
-            else:
-                words_found[current_word] = 1
-    return 0
 
 def count_words(phrase):
     '''
@@ -20,11 +8,15 @@ def count_words(phrase):
     Output: dictionary of words found with number of occurrences associated with each
     '''
     
-    words_found = {}
-
+    # Use regex to split the phrase by commas, underscores and whitespace.
+    # Strip any extra punctuation around the words and put everything in lowercase.
     words = [word.strip(string.punctuation).lower() for word in 
                 re.split(f"[,_{string.whitespace}]", phrase)]
+
+    # Build the count of each word in the phrase.
+    word_counter = collections.Counter()
     for word in words:
-        add_word_to_dict(words_found, word)
+        if word:
+            word_counter[word] += 1
         
-    return words_found
+    return dict(word_counter)
