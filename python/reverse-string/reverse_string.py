ddc4cbaffdfc6c551f234fc37bced015adc7d05d
xxylem <35666594+xxylem@users.noreply.github.com>
2019-10-12 20:30:25 +0100

    Simplify reverse_string to use slicing.

--- python/reverse-string/reverse_string.py
+++ python/reverse-string/reverse_string.py
@@ -1,14 +1,5 @@
-def reverse(input=''):
+def reverse(input_string=''):
     ''' Input is a string. Output is the reverse of that string.
     '''
-    if len(input) < 2:
-        # String length 0 or 1 is already reversed
-        return input
-    temp_string = ''
-    counter = 0
-    for i in range(len(input) -1, -1, -1):
-        # Go from index at last position of input string down to 0 and concatenate
-        # to a new string
-        temp_string += input[i]
-        counter += 1
-    return temp_string
+
+    return input_string[::-1]
