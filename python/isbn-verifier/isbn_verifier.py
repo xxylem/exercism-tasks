c3b941e196dce003008a3167b2716acb7eb7660d
xxylem <35666594+xxylem@users.noreply.github.com>
2019-10-12 21:46:34 +0100

    Tidy up rna_transcription and isbn_verifier (PY)

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,39 +1,30 @@
-def verify(isbn):
+def is_valid(isbn):
     ''' 
     Checks if a given ISBN-10 is valid.
     Input (string): ISBN to test
     Output (bool): True if valid, otherwise False
     '''
-    
-    # Constant values according to specification for ISBN-10
-    MODULUS = 11
-    VALUE_OF_CHAR_X = 10
-    ISBN_10_LENGTH = 10
-    INITIAL_MULTIPLIER = 10
-    
-    # Strip "-" character, that we can expect according to spec.
+       
+    # Check valid length and remove hyphen
     isbn = isbn.replace("-", "")
-    
-    # Refute invalid lengths immediately
-    if len(isbn) != ISBN_10_LENGTH:
+    if len(isbn) != 10:
         return False
-    
-    # Store running total for checksum formula
+
+    multiplicand = 10
     checksum = 0
-    # Store initial value of multiplier we will iterate down from
-    multiplier = INITIAL_MULTIPLIER
-    
     for char in isbn:
+
         if char.isdigit():
-            checksum += int(char) * multiplier
-        elif char == "X" and multiplier == 1:
-            # Only the last "digit" can be X: We can reuse the multiplier var
+            checksum += int(char) * multiplicand
+
+        elif char == "X" and multiplicand == 1:
+            # Only the last "digit" can be X: We can reuse the multiplicand var
             # to check for this
-            checksum += VALUE_OF_CHAR_X * multiplier
+            checksum += 10
+
         else:
             # Not a valid character
             return False
-        multiplier -= 1
+        multiplicand -= 1
     
-    # Returns bool
-    return checksum % MODULUS == 0
+    return checksum % 11 == 0
--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -2,22 +2,18 @@ def to_rna(dna_strand):
     ''' Transcribes input dna_strand (string) to RNA.
         Outputs this as a string
     '''
+
+    nucleotide_translations =   { 'G': 'C' 
+                                , 'C': 'G'
+                                , 'T': 'A'
+                                , 'A': 'U' }
+
     
-    # Empty string to write transcribed RNA strand to
     rna_strand = ''
     
-    # Iterate over the nucleotides in the DNA strand
     for nucleotide in dna_strand:
-        # Check for valid nucleotides and transcribe them
-        if nucleotide == 'G':
-            rna_strand += 'C'
-        elif nucleotide == 'C':
-            rna_strand += 'G'
-        elif nucleotide == 'T':
-            rna_strand += 'A'
-        elif nucleotide == 'A':
-            rna_strand += 'U'
-        # Any other value is invalid - raise exception
-        else:
+        if nucleotide not in nucleotide_translations:
             raise ValueError("Invalid DNA strand input. Please check")
+        rna_strand += nucleotide_translations[nucleotide]
+  
     return rna_strand
