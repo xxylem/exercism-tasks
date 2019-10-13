5ed5066dc06a8befebd1c26e40ba764ab8978613
xxylem <35666594+xxylem@users.noreply.github.com>
2019-10-13 09:49:35 +0100

    Use scientific notation.

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -3,7 +3,7 @@
 export const gigasecond = (datetime) => {
 
   // Datetimes are in milliseconds.
-  return new Date(datetime.getTime() + GIGASECOND * 1000)
+  return new Date(datetime.getTime() + GIGASECOND * 1e3)
 };
 
-const GIGASECOND = 1000000000
+const GIGASECOND = 1e9
