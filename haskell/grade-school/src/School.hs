3e34741218aeee5873a31a1e46db4ad9fe9d5dfe
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-15 10:54:56 +0100

    Names for each grade are now stored in sorted order.

--- haskell/grade-school/src/School.hs
+++ haskell/grade-school/src/School.hs
@@ -5,18 +5,21 @@ import qualified Data.IntMap.Strict as M
 
 type Grade = Int
 type Name = String
+
+-- INVARIANT: The names of each grade are stored in alphabetical
+--              order from A to Z.
 type School = M.IntMap [Name]
 
 
 add :: Grade -> Name -> School -> School
-add gradeNum name = M.insertWith (++) gradeNum [name]
+add gradeNum name = M.insertWith (\old new ->
+                        sort $ old ++ new) gradeNum [name]
 
 empty :: School
 empty = M.empty
 
 grade :: Grade -> School -> [Name]
-grade gradeNum school = sort $ M.findWithDefault [] gradeNum school
+grade = M.findWithDefault []
 
 sorted :: School -> [(Grade, [Name])]
-sorted school = map (\(g, names) ->
-                        (g, sort names))  (M.toAscList school)
+sorted = M.toAscList
