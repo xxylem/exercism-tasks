200d48d3b3cae0b76dea1be4ddb0e79fa215d1f8
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-15 21:09:28 +0100

    Clean up crypto-square (hs). Add nth-prime (HS)

--- haskell/crypto-square/src/CryptoSquare.hs
+++ haskell/crypto-square/src/CryptoSquare.hs
@@ -1,24 +1,40 @@
 module CryptoSquare (encode) where
 
-import Data.Char (isAlphaNum, toLower)
-import Data.Matrix (Matrix)
+import           Data.Char        (isAlphaNum, toLower)
+import           Data.Matrix      (Matrix)
 import qualified Data.Matrix as M
 import qualified Data.Vector as V (toList)
 
+-- Encode the message using the "CryptoSquare" system.
 encode :: String -> String
 encode message =
-    unwords $
-    toList $
-    M.fromList r c paddedMessage
-    where preProcessedMessage = map toLower $ filter isAlphaNum message
-          len = length preProcessedMessage
-          c = (ceiling . sqrt . fromIntegral) len
-          r = ceiling $ fromIntegral len / fromIntegral c
-          paddedMessage = preProcessedMessage ++ repeat ' '
 
-toList :: Matrix Char -> [String]
-toList m =
-    V.toList <$> go totalCols
-    where totalCols = M.ncols m
-          go 0 = []
-          go n = M.getCol (totalCols + 1 - n) m : go (n - 1)
+        -- Make 'chunks' from the columns
+        unwords 
+        -- Get the columns from the matrix
+    $   columnsToList 
+        -- Make a matrix from the message
+    $   M.fromList rows cols paddedMessage
+
+    where   preProcessedMessage = (map toLower . filter isAlphaNum) message
+            paddedMessage = preProcessedMessage ++ repeat ' '
+            (cols, rows) = (getDimensions . length) preProcessedMessage
+
+
+-- Returns a list of the columns of the matrix m starting
+--  from column 1.
+columnsToList :: Matrix Char -> [String]
+columnsToList m = V.toList <$> go totalCols
+    
+    where   totalCols = M.ncols m
+            go 0 = []
+            go n = M.getCol (totalCols + 1 - n) m : go (n - 1)
+
+
+-- Returns positive integers c and r 
+--      s.t. c*r >= n and c - r <= 1
+getDimensions :: Int -> (Int, Int)
+getDimensions n = (c, r)
+
+    where   c = (ceiling . (sqrt :: Double -> Double) . fromIntegral) n
+            r = ceiling (fromIntegral n / fromIntegral c :: Double)
