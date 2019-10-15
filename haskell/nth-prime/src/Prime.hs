839d6b6c12bbfc5345aa57a187c2982ee78e3f2f
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-15 22:04:52 +0100

    Rough implementation of nth prime completed, will improve tomorrow.

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -1,4 +1,24 @@
 module Prime (nth) where
 
+import Prelude hiding (gcd) 
+-- Problem statement forbids prime-related library functions.
+
 nth :: Int -> Maybe Integer
-nth n = error "You need to implement this function."
+nth 0 = Nothing
+nth n = Just $ primes !! n
+
+
+primes :: [Integer]
+primes = filter isPrime [1..]
+
+isPrime :: Integer -> Bool
+isPrime n =
+    and [ n `relativelyPrimeTo` x | x <- [2..n-1]]
+
+relativelyPrimeTo :: Integer -> Integer -> Bool
+relativelyPrimeTo a b =
+    gcd a b == 1
+
+gcd :: Integer -> Integer -> Integer
+gcd a 0 = a
+gcd a b = gcd b $ a `rem` b
