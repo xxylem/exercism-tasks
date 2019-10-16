11cffeb45e28118e477bee88d6546c70eccfeeba
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-16 11:23:51 +0100

    Move constant to top of file and capitalise.

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,11 +1,14 @@
+FACTOR_MESSAGES = [
+    (3, "Pling"),
+    (5, "Plang"),
+    (7, "Plong")]
+
+
 def convert(number):
     """ Converts number to string representation of its "sound",
         using messages already provided to add_new_factor_message."""
 
-    return "".join([message if number % factor == 0 else "" for (factor, message) in factor_messages]) or str(number)
+    return "".join([message if number % factor == 0 else "" for (factor, message) in FACTOR_MESSAGES]) or str(number)
 
 
-factor_messages = [
-    (3, "Pling"),
-    (5, "Plang"),
-    (7, "Plong")]
+
