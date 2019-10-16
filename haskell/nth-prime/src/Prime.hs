729facc8c7b295172e9dcb0901046c449c63e88a
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-16 09:14:02 +0100

    Update raindrops (py) per mentor feedback
    Refactor: generalise to allow any messages with any factors.

--- haskell/nth-prime/src/Prime.hs
+++ haskell/nth-prime/src/Prime.hs
@@ -11,6 +11,16 @@ nth n = Just $ primes !! n
 primes :: [Integer]
 primes = filter isPrime [1..]
 
+-- TODO this works but is very slow. 
+-- Leverage the already-computed primes:
+    -- We know:
+        -- If n divides any primes < n, n is not prime
+        -- So we can disregards all multiples of
+            -- 2
+            -- 3
+            -- 5
+            -- etc
+        -- once we know that they are primes.
 isPrime :: Integer -> Bool
 isPrime n =
     and [ n `relativelyPrimeTo` x | x <- [2..n-1]]
--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,20 +1,16 @@
 def convert(number):
     """ Converts number to string representation of its "sound",
-        s.t. if number:
-            has 3 as a factor, add 'Pling' to the result.
-            has 5 as a factor, add 'Plang' to the result.
-            has 7 as a factor, add 'Plong' to the result.
-            does not have any of 3, 5, or 7 as a factor, the result should be the digits of the number."""
+        using messages already provided to add_new_factor_message."""
 
-    sound = ""
+    return "".join([fac_fun(number) for fac_fun in factor_message_functions]) or str(number)
 
-    if number % 3 == 0:
-        sound += "Pling"
-    if number % 5 == 0:
-        sound += "Plang"
-    if number % 7 == 0:
-        sound += "Plong"
-    if not sound:
-        sound = str(number)
 
-    return sound
+def add_new_factor_message(factor, message):
+    factor_message_functions.append(
+        lambda x: message if x % factor == 0 else "")
+
+
+factor_message_functions = []
+add_new_factor_message(3, "Pling")
+add_new_factor_message(5, "Plang")
+add_new_factor_message(7, "Plong")
