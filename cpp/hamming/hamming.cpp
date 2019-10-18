8bf3fa919a779af94bd28a38451c1b56aa83dec8
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-18 21:18:09 +0100

    Use size_t instead of int for the length. Remove unnecessary braces.

--- cpp/hamming/hamming.cpp
+++ cpp/hamming/hamming.cpp
@@ -5,18 +5,15 @@ namespace hamming {
 
 	int compute(std::string left_strand, std::string right_strand) {
 
-		if (left_strand.length() != right_strand.length()) {
+		if (left_strand.length() != right_strand.length())
 			throw std::domain_error("Strands must have the same length");
-		}
 
-		int len = left_strand.length();
+		size_t len = left_strand.length();
 		int distance = 0;
 		
-		for (int i = 0; i < len; i++) {
-			if (left_strand[i] != right_strand[i]) {
+		for (int i = 0; i < len; i++)
+			if (left_strand[i] != right_strand[i])
 				distance++;
-			}
-		}
 		
 		return distance;
 	}
