5a1e33ade37dcec5c0aef5c05ed537724a6ba6a7
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-19 14:12:03 +0100

    Add grade-school (C++)

--- cpp/nucleotide-count/nucleotide_count.cpp
+++ cpp/nucleotide-count/nucleotide_count.cpp
@@ -5,21 +5,23 @@ using namespace std;
 
 namespace nucleotide_count {
 
-	counter::counter(string dna_strand) {
-
+	counter::counter(string dna_strand) 
+	{
 		m_nucleotide_count = { {'A', 0}
 							 , {'T', 0}
 							 , {'C', 0}
 							 , {'G', 0} };
 		
-		for (char nucleotide : dna_strand) {
+		for (char nucleotide : dna_strand) 
+		{
 			if (!is_valid_nucleotide(nucleotide))
 				throw invalid_argument("Strand contains an invalid nucleotide.");
 			m_nucleotide_count[nucleotide]++;
 		}
 	}
 
-	std::map<char, int> counter::nucleotide_counts(void) const {
+	std::map<char, int> counter::nucleotide_counts(void) const 
+	{
 		return m_nucleotide_count;
 	}
 
