0f77ce604d1ce7d7e030f491604029f20c1dc73d
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-19 14:00:22 +0100

    Complete nucleotide count (C++)

--- cpp/nucleotide-count/nucleotide_count.cpp
+++ cpp/nucleotide-count/nucleotide_count.cpp
@@ -1,5 +1,39 @@
 #include "nucleotide_count.h"
+#include <stdexcept>
+
+using namespace std;
 
 namespace nucleotide_count {
+
+	counter::counter(string dna_strand) {
+
+		m_nucleotide_count = { {'A', 0}
+							 , {'T', 0}
+							 , {'C', 0}
+							 , {'G', 0} };
+		
+		for (char nucleotide : dna_strand) {
+			if (!is_valid_nucleotide(nucleotide))
+				throw invalid_argument("Strand contains an invalid nucleotide.");
+			m_nucleotide_count[nucleotide]++;
+		}
+	}
+
+	std::map<char, int> counter::nucleotide_counts(void) const {
+		return m_nucleotide_count;
+	}
+
+	int counter::count(const char nucleotide) const
+	{
+		if (!is_valid_nucleotide(nucleotide))
+			throw invalid_argument("That is not a valid nucleotide.");
+
+		return m_nucleotide_count.at(nucleotide);
+	}
+
+	bool counter::is_valid_nucleotide(const char nucleotide) const
+	{
+		return valid_nucleotides.find(nucleotide) != string::npos;
+	}
 
 }  // namespace nucleotide_count
--- cpp/nucleotide-count/nucleotide_count.h
+++ cpp/nucleotide-count/nucleotide_count.h
@@ -1,7 +1,26 @@
 #if !defined(NUCLEOTIDE_COUNT_H)
 #define NUCLEOTIDE_COUNT_H
 
+#include <map>
+#include <string>
+
 namespace nucleotide_count {
+
+	class counter
+	{
+	public:
+
+		counter(std::string dna_strand);
+		std::map<char, int> nucleotide_counts(void) const;
+		int count(const char nucleotide) const;
+
+	private:
+
+		std::map<char, int> m_nucleotide_count;
+		const std::string valid_nucleotides = "ATCG";
+		bool is_valid_nucleotide(const char nucleotide) const;
+
+	};
 
 }  // namespace nucleotide_count
 
