6d7effd5ecdf4710c151958bde66bcceaf963cf8
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-19 14:51:47 +0100

    Implement grade-school (C++)

--- cpp/grade-school/grade_school.cpp
+++ cpp/grade-school/grade_school.cpp
@@ -1,5 +1,42 @@
 #include "grade_school.h"
+#include <algorithm>
+#include <stdexcept>
 
 namespace grade_school {
+
+	using namespace std;
+
+	/** Returns: List of all grades in the school, each grade being a list of students
+				 in that grade. */
+	map <int, vector<string>> school::roster() const 
+	{
+		return _roster;
+	}
+
+	void school::add(std::string name, int grade)
+	{
+		// INVARIANT: Students are added to the grade roster s.t. they are
+		//			  stored alphabetically.
+		vector<string>* grade_roster = &_roster[grade];
+		(*grade_roster).insert
+		(
+			std::upper_bound((*grade_roster).begin(), (*grade_roster).end(), name),
+			name
+		);
+	}
+
+	/** Returns: sorted list of students in the given grade. */
+	std::vector<std::string> school::grade(int g) const
+	{
+		try
+		{
+			return _roster.at(g);
+		}
+		catch (out_of_range& const e)
+		{
+			return {};
+		}
+		
+	}
 
 }  // namespace grade_school
--- cpp/grade-school/grade_school.h
+++ cpp/grade-school/grade_school.h
@@ -1,7 +1,28 @@
 #if !defined(GRADE_SCHOOL_H)
 #define GRADE_SCHOOL_H
 
+#include <map>
+#include <string>
+#include <vector>
+
 namespace grade_school {
+
+	class school
+	{
+	public:
+
+		// Names of students are stored together in their grade.
+		std::map <int, std::vector<std::string>> roster() const;
+		void add(std::string name, int grade);
+		std::vector<std::string> grade(int g) const;
+
+	private:
+
+		std::map <int, std::vector<std::string>> _roster;
+
+	};
+
+	
 
 }  // namespace grade_school
 
