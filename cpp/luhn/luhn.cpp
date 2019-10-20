db0e8eb2568cf57bc20c458298a6e12e320f9aeb
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-20 15:36:53 +0100

    Remove unnecessary includes.

--- cpp/luhn/luhn.cpp
+++ cpp/luhn/luhn.cpp
@@ -1,6 +1,4 @@
 #include "luhn.h"
-#include <vector>
-#include <iostream>
 
 namespace luhn {
 
@@ -11,12 +9,15 @@ namespace luhn {
 		const size_t len = number.size();
 		if (len <= 1)
 			return false;
-			
+
 		int sum = 0;
 		int num_digits_seen = 0;
-		bool double_this_digit = false;
+		bool double_this_digit = false;  // Every second digit from the right is doubled.
 
-		for (int i = (int) len - 1; i >= 0; i--)
+		// Start at end of string so we can compute the sum as we go.
+		// We do not know how many digits are in the string so do not know which
+		// digits need to be doubled if we start at the beginning.
+		for (int i = (int)len - 1; i >= 0; i--)
 		{
 			const char& c = number.at(i);
 
@@ -29,18 +30,20 @@ namespace luhn {
 					digit *= 2;
 					if (digit > 9)
 						digit -= 9;
-				}	
-				
+				}
+
 				sum += digit;
 				double_this_digit = !double_this_digit;
 				num_digits_seen++;
 			}
+
 			else if (c == ' ')
 				continue;
+
 			else
 				return false;
 		}
-		
+
 		return num_digits_seen > 1 && (sum % 10) == 0;
 	}
 
