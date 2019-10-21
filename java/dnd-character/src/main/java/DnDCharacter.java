b0b29e816f0e9e8e1828a734d8d91d3444684cad
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-21 15:01:15 +0100

    Implement dnd-character (Java)

---
+++ java/dnd-character/src/main/java/DnDCharacter.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+import java.util.stream.IntStream;
+
+class DnDCharacter {
+
+    static private final Random r = new Random();
+
+    private int strength;
+    private int dexterity;
+    private int constitution;
+    private int intelligence;
+    private int wisdom;
+    private int charisma;
+    private int hitpoints;
+
+    DnDCharacter() {
+        strength = ability();
+        dexterity = ability();
+        constitution = ability();
+        intelligence = ability();
+        wisdom = ability();
+        charisma = ability();
+        hitpoints = 10 + modifier(constitution);
+    }
+
+    int ability() {
+        IntStream dice = r.ints(4, 1, 6);
+        dice = dice.sorted();
+        dice = dice.skip(1);
+        return dice.sum();
+    }
+
+    int modifier(int input) { return Math.floorDiv(input - 10, 2); }
+
+    // Getters
+    int getStrength() { return strength; }
+    int getDexterity() { return dexterity; }
+    int getConstitution() { return constitution; }
+    int getIntelligence() { return intelligence; }
+    int getWisdom() { return wisdom; }
+    int getCharisma() { return charisma; }
+    int getHitpoints() { return hitpoints; }
+}
