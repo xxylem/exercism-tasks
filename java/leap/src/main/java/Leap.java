45a631830d29a27e3bb502be284940237e760cbf
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-21 13:05:36 +0100

    Implement leap (JAVA)

---
+++ java/leap/src/main/java/Leap.java
@@ -0,0 +1,10 @@
+class Leap {
+
+    boolean isLeapYear(int year) {
+        if (year % 400 == 0)
+            return true;
+        if (year % 100 == 0)
+            return false;
+        return year % 4 == 0;
+    }
+}
