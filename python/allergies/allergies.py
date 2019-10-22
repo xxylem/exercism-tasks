6c0960ad337b2048cff4147f083bc9487d148dd0
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-22 21:59:34 +0100

    Implement allergies (Python)

--- python/allergies/allergies.py
+++ python/allergies/allergies.py
@@ -1,11 +1,18 @@
 class Allergies(object):
 
+    ALLERGY_KEYS = ("cats", "pollen", "chocolate", "tomatoes", "strawberries",
+                    "shellfish", "peanuts", "eggs")
+
     def __init__(self, score):
-        pass
+        # Use equivalence to 8-bit strings, where 1 in the ith bit means
+        # person has allergy at ALLERGY_KEYS[i].
+        allergy_values = [bool(int(x)) for x in format(score, '08b')][-8:]
+        self.allergies = dict(zip(self.ALLERGY_KEYS, allergy_values))
 
     def is_allergic_to(self, item):
-        pass
+        return self.allergies[item]
 
     @property
     def lst(self):
-        pass
+
+        return [allergy for (allergy, hasAllergy) in self.allergies.items() if hasAllergy]
