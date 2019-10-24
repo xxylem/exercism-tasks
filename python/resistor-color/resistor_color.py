13d25cc0fa17fdcaa25cede12622ad5c927c8c48
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-24 11:57:36 +0100

    Implement resistor-color (Python)

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,20 @@
+COLOR_CODES = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9,
+}
+
+
+def color_code(color):
+    return COLOR_CODES[color]
+
+
+def colors():
+    return list(COLOR_CODES.keys())
