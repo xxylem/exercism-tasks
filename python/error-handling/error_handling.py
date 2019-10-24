7a0af0edea45f7fd367f16710fe7e20660f6d777
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-24 11:20:06 +0100

    Use with statement

--- python/error-handling/error_handling.py
+++ python/error-handling/error_handling.py
@@ -13,7 +13,5 @@ def handle_error_by_returning_tuple(input_data):
 
 
 def filelike_objects_are_closed_on_exception(filelike_object):
-    try:
+    with filelike_object:
         filelike_object.do_something()
-    finally:
-        filelike_object.close()
