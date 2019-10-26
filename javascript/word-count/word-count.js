424294fe17c527a06e9447148df96bf036f666ae
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-26 11:34:39 +0100

    Use hasOwnProperty()

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -3,11 +3,11 @@ export class Words {
   count(words) {
 
     let word_count = {}
-    const split_words = words.toLowerCase().trim().split(/[\t\n ]+/);
+    const split_words = words.toLowerCase().trim().split(/\s+/);
     
     for (const word of split_words) {
 
-      if (typeof word_count[word] === "number")
+      if (word_count.hasOwnProperty(word))
         word_count[word]++;
 
       else
