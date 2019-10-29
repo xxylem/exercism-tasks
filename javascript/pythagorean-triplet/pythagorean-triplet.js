e24d35a594f82c6a5ae6aeb3a3cdf894d0096f6c
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-29 20:30:02 +0000

    Implement pythagorean-triplet (JS)

---
+++ javascript/pythagorean-triplet/pythagorean-triplet.js
@@ -0,0 +1,49 @@
+const tolerance = 0.00001
+
+export class Triplet {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  sum() {
+    return this.a + this.b + this.c;
+  }
+
+  product() {
+    return this.a * this.b * this.c;
+  }
+
+  isPythagorean() {
+    return Math.abs(Math.hypot(this.a, this.b) - this.c) < tolerance;
+  }
+
+  static where({ minFactor: minFactor = 0,
+                 maxFactor: maxFactor,
+                 sum : sumCondition }) {
+    
+    const triplets = [];
+    const ignoreSumCondition = sumCondition === undefined;
+
+    // This is a very straightforward solution but it seems
+    // sufficient for the given test cases, which complete quickly.
+    // If speed becomes a problem, I could implement the divide-and-
+    // conquer search for triplets that I did for the Python version
+    // of this problem.
+    for (let a = minFactor; a < maxFactor - 1; a++) {
+      for (let b = a + 1; b < maxFactor; b++) {
+        for (let c = b + 1; c <= maxFactor; c++) {
+
+          const triplet = new Triplet(a, b, c);
+
+          if (triplet.isPythagorean() && 
+            (ignoreSumCondition || triplet.sum() === sumCondition))
+            triplets.push(triplet);
+        }
+      }
+    }
+
+    return triplets;
+  }
+}
