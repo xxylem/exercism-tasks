8d6144e360dc0b6d6d9ae0adbe8f148522757548
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-10-29 08:38:13 +0000

    Implement scrabble-score (JS)

--- javascript/scrabble-score/scrabble-score.js
+++ javascript/scrabble-score/scrabble-score.js
@@ -1,8 +1,24 @@
-//
-// This is only a SKELETON file for the 'Scrabble Score' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// ASSUMES: letter is a member of the upper-case English alphabet.
+const getLetterScore = (letter) => {
 
-export const score = () => {
-  throw new Error("Remove this statement and implement this function");
+  switch (letter) {
+      case 'Q': case 'Z':
+        return 10;
+      case 'J': case 'X':
+        return 8;
+      case 'K':
+        return 5;
+      case 'F': case 'H': case 'V': case 'W': case 'Y':
+        return 4;
+      case 'B': case 'C': case 'M': case 'P':
+        return 3;
+      case 'D': case 'G':
+        return 2;
+      default:
+        return 1; 
+  }
+}
+
+export const score = (word) => {
+  return [...word.toUpperCase()].map(getLetterScore).reduce((a, b) => a + b, 0);
 };
