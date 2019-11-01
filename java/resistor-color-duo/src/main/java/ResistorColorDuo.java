7e8a4071c809c8eb0b011b5d3a9b720b23d94d96
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-11-01 16:56:36 +0000

    Remove old comments.

--- java/resistor-color-duo/src/main/java/ResistorColorDuo.java
+++ java/resistor-color-duo/src/main/java/ResistorColorDuo.java
@@ -1,17 +1,3 @@
-//
-//Black: 0
-//        Brown: 1
-//        Red: 2
-//        Orange: 3
-//        Yellow: 4
-//        Green: 5
-//        Blue: 6
-//        Violet: 7
-//        Grey: 8
-//        White: 9
-//
-
-
 import java.util.HashMap;
 import java.util.Map;
 
