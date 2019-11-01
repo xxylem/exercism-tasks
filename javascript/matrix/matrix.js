41ffe8cb57f273891631055aef7f5408a6da5215
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-11-01 13:51:22 +0000

    Implement matrix (js)

---
+++ javascript/matrix/matrix.js
@@ -0,0 +1,16 @@
+import { transpose } from 'mathjs'
+
+export class Matrix {
+  constructor(matrix_string) {
+    this.mat = matrix_string.split("\n")
+          .map(str => str.split(' ').map(Number));
+  }
+
+  get rows() {
+    return this.mat;
+  }
+
+  get columns() {
+    return transpose(this.mat);
+  }
+}
