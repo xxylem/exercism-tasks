3f2fd825418cdccd733671dfb2b691347933fa35
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-11-14 18:26:14 +0000

    Implement pangram (C#)

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        HashSet<char> lettersFound = new HashSet<char>();
+
+        foreach (char c in input)
+            if (char.IsLetter(c))
+                lettersFound.Add(char.ToLower(c));
+
+        return lettersFound.Count == 26;
+    }
+}
