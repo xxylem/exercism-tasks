3765d821ef707a6a06e93bdba5bdab3ae50be1be
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-11-14 10:02:21 +0000

    Improve pascals-triangle (HS) based on public solutions.
    Now use iterate and take. Handles negative input better.

--- haskell/pascals-triangle/src/Triangle.hs
+++ haskell/pascals-triangle/src/Triangle.hs
@@ -1,10 +1,5 @@
 module Triangle (rows) where
 
--- ASSUMES: n >= 0
 rows :: Int -> [[Integer]]
-rows 0 = []
-rows 1 = [[1]]
-rows n = restOfTriangle ++ [newRow]
-    where restOfTriangle = rows (n - 1)
-          lastRow = last restOfTriangle  -- Nonempty due to base case n=1
-          newRow = zipWith (+) (lastRow++[0]) (0:lastRow)
+rows n = take n $ iterate nextRow [1]
+    where nextRow prevRow = zipWith (+) (prevRow++[0]) (0:prevRow)
