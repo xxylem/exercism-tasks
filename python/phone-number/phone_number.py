045672a1c3689604bc4346100cac7fae20f27fd8
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-11-14 16:37:33 +0000

    Implement phone-number (Py)

---
+++ python/phone-number/phone_number.py
@@ -0,0 +1,19 @@
+import re
+
+
+class Phone:
+    def __init__(self, phone_number):
+        m = re.search(
+            r"^(?:\+?1)?\s*\(?(?P<area>[2-9]\d\d)\)?[. -]*(?P<exchange>[2-9]\d\d)[. -]*(?P<subscriber>\d{4})\s*$",
+            phone_number)
+
+        if m is None:
+            raise ValueError("Invalid number")
+
+        self.area_code = m.group("area")
+        self.exchange_code = m.group("exchange")
+        self.subscriber_code = m.group("subscriber")
+        self.number = self.area_code + self.exchange_code + self.subscriber_code
+
+    def pretty(self):
+        return f"({self.area_code}) {self.exchange_code}-{self.subscriber_code}"
