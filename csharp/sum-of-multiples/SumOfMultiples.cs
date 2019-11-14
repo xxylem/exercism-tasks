fd2f79b35d6523fd9842f03e41438c3f215bdfc4
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-11-14 20:21:12 +0000

    Implement sum-of-multiples (C#)

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> factors, int max)
+    {
+        var multiples = new HashSet<int>();
+        foreach (int factor in factors)
+        {
+            if (factor == 0)
+                continue;
+
+            for (int i = factor; i < max; i += factor)
+                multiples.Add(i);
+        }
+
+        return multiples.Sum();
+
+    }
+}
