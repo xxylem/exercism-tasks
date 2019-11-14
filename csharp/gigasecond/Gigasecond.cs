4311a3b104954e79d9af30ca7199230482baf8bd
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-11-14 19:43:34 +0000

    Update gigasecond per auto-mentor feedback

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -2,7 +2,7 @@ using System;
 
 public static class Gigasecond
 {
-    private static readonly int GIGASECOND = 1000000000;
+    private const int GIGASECOND = 1_000_000_000;
 
     public static DateTime Add(DateTime moment) => moment.AddSeconds(GIGASECOND);
 }
