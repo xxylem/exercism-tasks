ec4dbb94d3a870f8051024c7a76f9e6551fd9372
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-11-14 19:15:21 +0000

    Implement isogram (C#)

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        var lettersSeen = new HashSet<char>();
+
+        foreach (char c in word)
+            if (char.IsLetter(c))
+            {
+                char c_lower = char.ToLower(c);
+                if (lettersSeen.Contains(c_lower))
+                    return false;
+                else
+                    lettersSeen.Add(c_lower);
+            }
+
+        return true;
+    }
+}
