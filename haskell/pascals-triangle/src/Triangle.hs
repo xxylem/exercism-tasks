af6ad5e14a2bf34b0f4ebd366cc15b3ca38f8c4f
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-11-14 09:54:37 +0000

    Implement pascals-triangle (HS)

--- haskell/pascals-triangle/src/Triangle.hs
+++ haskell/pascals-triangle/src/Triangle.hs
@@ -1,4 +1,10 @@
 module Triangle (rows) where
 
+-- ASSUMES: n >= 0
 rows :: Int -> [[Integer]]
-rows x = error "You need to implement this function."
+rows 0 = []
+rows 1 = [[1]]
+rows n = restOfTriangle ++ [newRow]
+    where restOfTriangle = rows (n - 1)
+          lastRow = last restOfTriangle  -- Nonempty due to base case n=1
+          newRow = zipWith (+) (lastRow++[0]) (0:lastRow)
