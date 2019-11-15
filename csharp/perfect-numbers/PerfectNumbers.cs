6f0d618e4fb229d1b001db4711b3cb19dab62856
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-11-15 12:19:05 +0000

    Implement perfect-numbers (C#)

---
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,26 @@
+﻿public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        if (number < 1)
+            throw new System.ArgumentOutOfRangeException($"Only defined on positive integers: given {number}.");
+
+        int sum = 0;
+        for (int i = 1; i < number; i++)
+        {
+            if (number % i == 0)
+                sum += i;
+        }
+
+        return sum == number ? Classification.Perfect
+             : sum > number  ? Classification.Abundant
+             :                 Classification.Deficient;
+    }
+}
