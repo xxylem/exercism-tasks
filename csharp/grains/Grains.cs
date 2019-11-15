fd55a113a72fb87c4e70c8b0ea94be15e0fa76a3
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-11-15 11:20:25 +0000

    Implement grains (C#)

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,12 @@
+﻿public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if (n < 1 || n > 64)
+            throw new System.ArgumentOutOfRangeException($"Only defined on [1,64], given {n}.");
+
+        return 1UL << n - 1;
+    }
+
+    public static ulong Total() => 18_446_744_073_709_551_615UL;
+}
