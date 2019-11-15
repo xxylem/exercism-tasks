477b531fd6c9b7a983b40941185e5dcea53511ce
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-11-15 10:56:32 +0000

    Implement difference-of-squares (C#)

---
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int n)
+    {
+        int sum = (n * (n + 1)) / 2;
+        return sum * sum;
+    }
+    public static int CalculateSumOfSquares(int n) => ((2 * n + 1) * n * (n + 1)) / 6;
+
+    public static int CalculateDifferenceOfSquares(int n) => CalculateSquareOfSum(n) - CalculateSumOfSquares(n);
+}
