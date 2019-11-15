cd728153865a6ae75bfe9f871f90e107dd674fef
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-11-15 10:24:20 +0000

    Implement collatz-conjecture (C#)

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,15 @@
+﻿public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if (number < 1)
+            throw new System.ArgumentException("Only defined on positive integers.");
+
+        int Helper(int n) => 
+               n == 1     ? 0
+             : n % 2 == 0 ? 1 + Helper(n / 2)
+             :              1 + Helper(3 * n + 1);
+
+        return Helper(number);
+    }
+}
