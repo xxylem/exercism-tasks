0b7db0543dd4c7d17bf694acb2f720bb3300f137
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-11-15 15:13:30 +0000

    Implement pascals-triangle (C#)

---
+++ csharp/pascals-triangle/PascalsTriangle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class PascalsTriangle
+{
+    public static IEnumerable<IEnumerable<int>> Calculate(int rows)
+    {
+        int[] lastRow = new int[0];
+
+        for (int rowNumber = 1; rowNumber <= rows; rowNumber++)
+        {
+            int[] newRow = new int[lastRow.Length + 1];
+            newRow[0] = 1;
+            newRow[newRow.Length - 1] = 1;
+            
+            for (int i = 1; i < newRow.Length - 1; i++)
+            {
+                newRow[i] = lastRow[i - 1] + lastRow[i];
+            }
+
+            yield return newRow.AsEnumerable();
+            lastRow = newRow;
+        }
+    }
+
+}
