7157b80c0ed8071c5a4affe4c1274a82077b6f7d
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-11-18 17:37:29 +0000

    Implement raindrops (C#)

---
+++ csharp/raindrops/Raindrops.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+public static class Raindrops
+{
+    private static readonly ReadOnlyDictionary<int, string> DROPS = new ReadOnlyDictionary<int, string>
+        (
+            new Dictionary<int, string>()
+            {
+                { 3, "Pling" },
+                { 5, "Plang" },
+                { 7, "Plong" }
+            }
+        );
+
+    public static string Convert(int number)
+    {
+        var sb = new StringBuilder();
+        foreach (var drop in DROPS)
+        {
+            if (number % drop.Key == 0)
+                sb.Append(drop.Value);
+        }
+
+        return sb.Length > 0 ? sb.ToString() : number.ToString();
+    }
+}
