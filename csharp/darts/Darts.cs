8adf82bb8173141cb9df234a12e7bba8aa24e1a9
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-11-18 14:02:21 +0000

    Implement darts (C#)

---
+++ csharp/darts/Darts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+        double dist = Math.Sqrt(x * x + y * y);
+
+        return dist > 10 ? 0 
+             : dist > 5  ? 1 
+             : dist > 1  ? 5 
+             :             10;
+    }
+}
