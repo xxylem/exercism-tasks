1924dbc9b89b6846362f620717bd2628562cf377
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-11-18 13:09:42 +0000

    Add noexcepts to space-age (C++)

--- cpp/space-age/space_age.h
+++ cpp/space-age/space_age.h
@@ -14,17 +14,17 @@ namespace space_age {
 	class space_age
 	{
 	public:
-		constexpr space_age::space_age(long long int seconds) : _seconds(seconds) {}
-		constexpr long long int space_age::seconds() const { return _seconds; }
+		constexpr space_age::space_age(long long int seconds) noexcept : _seconds(seconds) {}
+		constexpr long long int space_age::seconds() const noexcept { return _seconds; }
 
-		constexpr double space_age::on_mercury() const { return _seconds / MERCURY_YEAR_IN_SECONDS; }
-		constexpr double space_age::on_venus()   const { return _seconds / VENUS_YEAR_IN_SECONDS; }
-		constexpr double space_age::on_earth()   const { return _seconds / EARTH_YEAR_IN_SECONDS; }
-		constexpr double space_age::on_mars()    const { return _seconds / MARS_YEAR_IN_SECONDS; }
-		constexpr double space_age::on_jupiter() const { return _seconds / JUPITER_YEAR_IN_SECONDS; }
-		constexpr double space_age::on_saturn()  const { return _seconds / SATURN_YEAR_IN_SECONDS; }
-		constexpr double space_age::on_uranus()  const { return _seconds / URANUS_YEAR_IN_SECONDS; }
-		constexpr double space_age::on_neptune() const { return _seconds / NEPTUNE_YEAR_IN_SECONDS; }
+		constexpr double space_age::on_mercury() const noexcept { return _seconds / MERCURY_YEAR_IN_SECONDS; }
+		constexpr double space_age::on_venus()   const noexcept { return _seconds / VENUS_YEAR_IN_SECONDS; }
+		constexpr double space_age::on_earth()   const noexcept { return _seconds / EARTH_YEAR_IN_SECONDS; }
+		constexpr double space_age::on_mars()    const noexcept { return _seconds / MARS_YEAR_IN_SECONDS; }
+		constexpr double space_age::on_jupiter() const noexcept { return _seconds / JUPITER_YEAR_IN_SECONDS; }
+		constexpr double space_age::on_saturn()  const noexcept { return _seconds / SATURN_YEAR_IN_SECONDS; }
+		constexpr double space_age::on_uranus()  const noexcept { return _seconds / URANUS_YEAR_IN_SECONDS; }
+		constexpr double space_age::on_neptune() const noexcept { return _seconds / NEPTUNE_YEAR_IN_SECONDS; }
 
 	private:
 		const long long int _seconds;
