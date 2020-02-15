8986a260b9dec85cab1bf648d68708ecc12e3111
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2020-02-15 16:39:59 +0000

    Implement bob

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,4 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Bob (responseFor) where
 
-responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+import           Data.Char (isLower, isSpace, isUpper)
+import           Data.Text (Text)
+import qualified Data.Text as T
+
+questionResponse, yellingResponse, yellingQuestionResponse, silenceReponse, defaultResponse :: Text
+questionResponse        = "Sure."
+yellingResponse         = "Whoa, chill out!"
+yellingQuestionResponse = "Calm down, I know what I'm doing!"
+silenceReponse          = "Fine. Be that way!"
+defaultResponse         = "Whatever."
+
+responseFor :: Text -> Text
+responseFor t
+    | yelling && question   = yellingQuestionResponse
+    | yelling               = yellingResponse
+    | question              = questionResponse
+    | isSilence t           = silenceReponse
+    | otherwise             = defaultResponse
+    where yelling  = isYelling t
+          question = isQuestion t
+
+isQuestion :: Text -> Bool
+isQuestion t = not (T.null strippedText) 
+            && T.last strippedText == '?'
+    where strippedText = T.stripEnd t
+
+isYelling :: Text -> Bool
+isYelling t = not (T.any isLower t) 
+           && T.any isUpper t
+
+isSilence :: Text -> Bool
+isSilence = T.all isSpace
