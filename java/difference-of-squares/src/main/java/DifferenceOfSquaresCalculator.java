b23de9eb3e7fe278e3209d289522dd9726828029
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2020-02-16 10:11:30 +0000

    Implement difference of squares

---
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -0,0 +1,16 @@
+class DifferenceOfSquaresCalculator {
+
+    int computeSquareOfSumTo(int input) {
+        int sum = (input * (input + 1)) / 2;
+        return sum * sum;
+    }
+
+    int computeSumOfSquaresTo(int input) {
+        return (input * (input + 1) * (2 * input + 1)) / 6;
+    }
+
+    int computeDifferenceOfSquares(int input) {
+        return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
+    }
+
+}
