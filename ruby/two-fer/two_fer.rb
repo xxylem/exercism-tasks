305b1e9a80adb6b4192e593eb6132999a8d1c445
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2020-04-09 12:19:46 +0100

    Fix two_fer per mentor advice.

--- ruby/two-fer/two_fer.rb
+++ ruby/two-fer/two_fer.rb
@@ -1,6 +1,5 @@
 class TwoFer
-
-    def self.two_fer(name = "you")
-        "One for #{name}, one for me."
-    end
+  def self.two_fer(name = "you")
+    format("One for %s, one for me.", name)
+  end
 end
