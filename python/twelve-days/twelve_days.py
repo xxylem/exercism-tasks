11b21c3e33769233fecdfcad8cae13ded56906f7
joshu <joshua.d.taylor@hotmail.co.uk>
2020-05-12 18:42:59 +0100

    Changes following mentor review.

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -4,15 +4,23 @@ def recite(start_verse, end_verse):
 
 # Assumes 1 <= number <= 12
 def verse(number):
-    if number == 1:
-        # Special case, easier just to hard-code.
-        return "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree."
+    return verse_start(number) + verse_end(number)
 
-    return "On the " + ordinal_numbers[number] + " day of Christmas my true love gave to me: " + \
-           "".join(gifts[number - 1::-1])
+
+def verse_start(number):
+    return "On the " + ordinal_numbers[number] + " day of Christmas my true love gave to me: "
+
+
+def verse_end(number):
+    gifts_str = ", ".join(gifts[number - 1:0:-1])
+    if number > 1:
+        gifts_str += ", and "
+    gifts_str += gifts[0] + '.'
+    return gifts_str
 
 
 ordinal_numbers = {
+    1: "first",
     2: "second",
     3: "third",
     4: "fourth",
@@ -27,16 +35,16 @@ ordinal_numbers = {
 }
 
 gifts = [
-    "and a Partridge in a Pear Tree.",
-    "two Turtle Doves, ",
-    "three French Hens, ",
-    "four Calling Birds, ",
-    "five Gold Rings, ",
-    "six Geese-a-Laying, ",
-    "seven Swans-a-Swimming, ",
-    "eight Maids-a-Milking, ",
-    "nine Ladies Dancing, ",
-    "ten Lords-a-Leaping, ",
-    "eleven Pipers Piping, ",
-    "twelve Drummers Drumming, "
+    "a Partridge in a Pear Tree",
+    "two Turtle Doves",
+    "three French Hens",
+    "four Calling Birds",
+    "five Gold Rings",
+    "six Geese-a-Laying",
+    "seven Swans-a-Swimming",
+    "eight Maids-a-Milking",
+    "nine Ladies Dancing",
+    "ten Lords-a-Leaping",
+    "eleven Pipers Piping",
+    "twelve Drummers Drumming"
 ]
